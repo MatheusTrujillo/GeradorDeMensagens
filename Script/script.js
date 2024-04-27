9421f3deedc16bbd7e1cdee1162ab3ead668d007
Matheus Trujillo de oliveira <matheustrujillodev@gmail.com>
2024-04-27 18:50:24 -0300

    Changes v0.4.1

--- Script/script.js
+++ Script/script.js
@@ -96,3 +96,13 @@ function enviarWhatsapp() {
     // Abre a URL do WhatsApp em uma nova aba
     window.open(linkWhatsapp, '_blank');
 }
+
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementsByClassName("content")[0].style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementsByClassName("content")[0].style.marginLeft= "0";
+}
